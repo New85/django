4c2c0fb26deb5388cf162c284ef70083695f3d3a
Uriy <102480221+New85@users.noreply.github.com>
2023-11-10 16:30:25 +0300

    ferst commit

--- cars/car/urls.py
+++ cars/car/urls.py
@@ -7,8 +7,9 @@ register_converter(convertors.FourDigitYearConverter, "year4")
 
 urlpatterns = [
 
-    path('', views.index),
-    path('story/<int:car_id>', views.categories),
+    path('', views.index, name='home'),  # http://127.0.0.1:8000/
+    path('about/', views.about, name='about'),
+    path('story/<int:car_id>', views.categories, name='car_id'),
     path('story/<slug:car_slug>/', views.categories_bu_slug),
     path("archive/<year4:year>/",views.archive),
 
--- cars/car/views.py
+++ cars/car/views.py
@@ -1,10 +1,36 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, request
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+
 
 # Create your views here.
 
-def index(reguest):
-    return HttpResponse('Страница Car')
+menu =['Осайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+class MyClass:
+    def __int__(self,a , b):
+        self.a = a
+        self.b = b
+
+
+
+def index(request):
+    # t = render_to_string('car/index.html')  # путь к шаблону
+    # return HttpResponse(t)
+    data = {'titel': 'Главная страница',
+            'menu': menu,
+            'float': 255.25,
+            'lst': [1,2,'abc', True],
+            'set': {1,2,3,5},
+            'dict': {'key1': 'value1', 'key2': 'value2'},
+            'obj': MyClass(10,20)
+
+            }
+    return render(request,'car/index.html',context=data) # более удобный путь
+
+def about(request):
+    return render(request, 'car/about.html',{'titel': 'О сайте'})
 
 def categories(request, car_id):
     return HttpResponse(f"<h1>Страница HISTORIC CARS</h1><p>{car_id}</p>")
@@ -16,9 +42,11 @@ def categories_bu_slug(request, car_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+
+        return redirect('/') # перенаправление на главную страницу
+
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
-def page_not_found(request,exception):
+def page_not_found(request,exception):  # ошибка 404 более красивая
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
